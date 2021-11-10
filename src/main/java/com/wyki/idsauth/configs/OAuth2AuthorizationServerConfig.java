7f64a1680453b2c3fa1b67d481dd24321bde31cf
wycliffmuriithi@gmail.com <wycliffmuriithi@gmail.com>
2021-11-10 11:23:59 +0300

    preparing for release

--- src/main/java/com/wyki/idsauth/configs/OAuth2AuthorizationServerConfig.java
+++ src/main/java/com/wyki/idsauth/configs/OAuth2AuthorizationServerConfig.java
@@ -3,10 +3,10 @@ package com.wyki.idsauth.configs;
 import com.wyki.idsauth.services.UserService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
@@ -20,7 +20,6 @@ import org.springframework.security.oauth2.provider.token.TokenEnhancerChain;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
-import org.springframework.security.oauth2.provider.token.store.KeyStoreKeyFactory;
 
 import javax.sql.DataSource;
 import java.util.Arrays;
@@ -39,6 +38,9 @@ public class OAuth2AuthorizationServerConfig extends AuthorizationServerConfigur
     private DataSource dataSource;
     @Autowired
     UserService userService;
+
+    @Value("${mpm.auth.signingkey}")
+    String signingkey;
 
 
     @Override
@@ -80,11 +82,11 @@ public class OAuth2AuthorizationServerConfig extends AuthorizationServerConfigur
     @Bean
     public JwtAccessTokenConverter accessTokenConverter() {
         JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
-        ClassPathResource resource = new ClassPathResource("10.153.1.54.jks");
-//        LOGGER.info("resource path "+resource.getPath());
-        final KeyStoreKeyFactory keyStoreKeyFactory = new KeyStoreKeyFactory(resource, "10.153.1.54".toCharArray());
-        converter.setKeyPair(keyStoreKeyFactory.getKeyPair("10.153.1.54"));
-//        converter.setSigningKey("ER@Ds32_32d4");
+//        ClassPathResource resource = new ClassPathResource("10.153.1.54.jks");
+////        LOGGER.info("resource path "+resource.getPath());
+//        final KeyStoreKeyFactory keyStoreKeyFactory = new KeyStoreKeyFactory(resource, "10.153.1.54".toCharArray());
+//        converter.setKeyPair(keyStoreKeyFactory.getKeyPair("10.153.1.54"));
+        converter.setSigningKey(signingkey);
         return converter;
     }
 
